543e727f597297348a34ff5a543f4bac43c392ad
Ashok <gcpcloud754@gmail.com>
2025-08-15 10:56:36 +0530

    test

--- workflows/pyspark_dag.py
+++ workflows/pyspark_dag.py
@@ -13,7 +13,7 @@ from airflow.providers.google.cloud.operators.dataproc import (
 PROJECT_ID = "prime-byway-465304-a3"
 REGION = "us-east1"
 CLUSTER_NAME = "my-demo-cluster"
-COMPOSER_BUCKET = "us-central1-airflow-4776f681-bucket" ## change this 
+COMPOSER_BUCKET = "us-central1-airflow-4776f681-bucket" ## change thi 
 
 GCS_JOB_FILE_1 = f"gs://{COMPOSER_BUCKET}/data/INGESTION/hospitalA_mysqlToLanding.py"
 PYSPARK_JOB_1 = {
